7153df8b6a8412cd4f6479299c80ade6c1c73541
GoldenXLence <jack@koskie.ca>
2021-11-11 19:40:23 -0500

    Added invite command

---
+++ src/commands/invite.ts
@@ -0,0 +1,15 @@
+import { Command, PieceContext } from "@sapphire/framework";
+import { Message } from "discord.js";
+import { client } from "..";
+
+export default class InviteCommand extends Command {
+    constructor(context: PieceContext) {
+        super(context, {
+            description: 'A link to add the bot to your own server.'
+        })
+    }
+
+    messageRun(message: Message) {
+        message.reply(`https://discordapp.com/oauth2/authorize?client_id=${client?.user?.id}&permissions=2147609681&scope=applications.commands%20bot`);
+    }
+}
